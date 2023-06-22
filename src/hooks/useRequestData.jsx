6c2d4d56d7d96c3894ff7b5d1b5f3a5fee87e8ba
Julia-slvbrg <julia.slvb@gmail.com>
2023-06-21 23:13:13 -0300

    Developed: hook to fetch info from the api, react context and initial data fetched

---
+++ src/hooks/useRequestData.jsx
@@ -0,0 +1,23 @@
+import axios from "axios";
+import { useEffect, useState } from "react"
+import { BASE_URL } from "../constants/BASE_URL";
+
+export const useRequestData = (endpoint, initialState) => {
+    const [filmListFromAPI, setFilmListFromAPI] = useState(initialState);
+
+    useEffect(()=>{
+        axios.get(`${BASE_URL}${endpoint}`)
+        .then((response)=>{
+               
+            //console.log(response.data)
+            setFilmListFromAPI(response.data)
+        })
+        .catch ((error)=>{
+            console.log(error)
+        })
+        
+    },[endpoint])
+    
+    return[filmListFromAPI]
+}
+
