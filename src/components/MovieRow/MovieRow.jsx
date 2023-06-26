61226810ab36aebb1e21abaae55c11c70b4a8db5
Julia-slvbrg <julia.slvb@gmail.com>
2023-06-25 21:33:05 -0300

    feat: basic style for MovieRow

--- src/components/MovieRow/MovieRow.jsx
+++ src/components/MovieRow/MovieRow.jsx
@@ -1,18 +1,25 @@
+import { CategoryTitle, Img, ItemContainer, ListAreaContainer, ListContainer, MovieRowContainer } from "./MovieRowStyle"
+
 export const MovieRow = ({title, items}) => {
     //console.log({title})
     //console.log({items})
     return(
-        <div>
-            <h2>{title}</h2>
-            <div>
-                {items? items.results.map((item, index)=>{
-                    return(
-                        <img src={`https://image.tmdb.org/t/p/w300/${item.poster_path}`} alt={item.original_title} key={index}/> 
-                    )
-                } ): null}
-            </div>
+        <MovieRowContainer>
+            <CategoryTitle>{title}</CategoryTitle>
+            <ListAreaContainer>
+                <ListContainer>
+                    {items? items.results.map((item, index)=>{
+                        return(
+                            <ItemContainer key={index}>
+                                <Img src={`https://image.tmdb.org/t/p/w300/${item.poster_path}`} alt={item.original_title}/> 
+                            </ItemContainer>    
+                        )
+                    } ): null}
+                </ListContainer>
+                
+            </ListAreaContainer>
 
-        </div>
+        </MovieRowContainer>
     )
 }
 
