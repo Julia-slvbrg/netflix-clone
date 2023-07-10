664f3f39f25dcc0b0864eefe3147e636ad588a5e
Julia-slvbrg <julia.slvb@gmail.com>
2023-07-09 23:02:28 -0300

    feat: footer component basic structure

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,3 +1,4 @@
 export * from "./FeaturedMovie";
 export * from "./Header";
 export * from "./MovieRow";
+export * from "./Footer"
--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,24 +1,13 @@
 import { useContext, useEffect, useState } from "react"
 import { GlobalContext } from "../../context/GlobalContext"
-import { MovieRow, FeaturedMovie, Header} from "../../components"
+import { MovieRow, FeaturedMovie, Header, Footer} from "../../components"
 import { Lists } from "./HomePageStyle";
 
 
 function HomePage () {
   const context = useContext(GlobalContext);
-  const { moviesList, setMoviesList, featuredData } = context;
-  const [blackHeader, setBlackHeader] = useState(false);
-
-  useEffect(()=>{
-    const scrollListener = () => {
-      window.scrollY > 10? setBlackHeader(true) : setBlackHeader(false)
-    }
-    window.addEventListener('scroll', scrollListener);
-
-    return () => {
-      window.removeEventListener('scroll', scrollListener);
-    }
-  }, [])
+  const { moviesList, featuredData, blackHeader } = context;
+  
   
 
   return(
@@ -34,6 +23,7 @@ function HomePage () {
             />
           )}
           </Lists>
+          <Footer/>
       </>
   )
 }
